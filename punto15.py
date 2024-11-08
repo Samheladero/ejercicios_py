707f8e57b743db895f269c622098ab2651df3411
samheladero <castrillonmedinasamuel@gmail.com>
2024-11-08 10:25:08 -0500

    cambio punto15

--- punto15.py
+++ punto15.py
@@ -2,7 +2,10 @@ import math
 
 def ecuacion_cuadratica(a, b, c):
     discriminante = b**2 - 4*a*c
-    if discriminante > 0:
+    if a == 0:
+        print ("NO puede ser 0") 
+    
+    elif discriminante > 0:
         raiz1 = (-b + math.sqrt(discriminante)) / (2*a)
         raiz2 = (-b - math.sqrt(discriminante)) / (2*a)
         print("Las raíces son:", raiz1, " y ", raiz2)
